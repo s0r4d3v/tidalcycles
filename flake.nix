bca7954fc4b00b2d28080ab3801a38af47d92087
s0r4d3v <s0r4d3v@gmail.com>
2026-02-18 01:41:29 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Cross-platform Nix development template with direnv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Add your development packages here
+            ];
+            shellHook = ''
+              echo "Hello, world!"
+            '';
+          };
+        };
+    };
+}
